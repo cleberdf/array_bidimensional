df1f409c0dce5673c68435cc3283a0c8225292d2
bill <cleber.flauzino@hotmail.com>
2023-01-24 00:00:19 -0300

    first commit

---
+++ src/ArrayBidimensional.java
@@ -0,0 +1,15 @@
+//array bidimensional, programa que mostra a quantidade de elementos de duas matrizes
+public class ArrayBidimensional {
+
+  public static void main(String[] args) {
+
+    int[][] myNumber = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+
+    for (int i = 0; i < myNumber.length; i++) {
+      for (int j = 0; j < myNumber[i].length; ++j) {
+
+        System.out.println([i][j]);
+      }
+    }
+  }
+}
